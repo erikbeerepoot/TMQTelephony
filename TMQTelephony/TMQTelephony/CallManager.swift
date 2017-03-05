fa7ef17a62f0577c99e44c16b26210409462c761
Erik Beerepoot <erikbeerepoot@hush.com>
2017-03-05 12:22:04 -0800

    Compile time DI for OS dependent call fw. Also hook up basic calling functionality to callkit implementation

--- TMQTelephony/TMQTelephony/CallManager.swift
+++ TMQTelephony/TMQTelephony/CallManager.swift
@@ -12,7 +12,7 @@ import CleanroomLogger
 import TwilioVoiceClient
 
 ///Abstract interface to hide call SDK details, as they differ for different versions of iOS
-protocol SystemTelephonyAdapter {
+public protocol SystemTelephonyAdapter {
     func handleIncomingCall(_ : Call) throws
     func startCall(_ : Call) throws
     func stopCall(_ : Call) throws
@@ -28,6 +28,8 @@ public class CallManager : NSObject, CallDelegate {
     ///Get list of all incoming calls, in reverse chronological order
     private(set) var  incomingCalls : [Call] = []
     
+    private(set) var telephonyService : SystemTelephonyAdapter? = nil
+    
     ///Get list of all outgoing and incoming calls, in reverse chronological order
     public var calls : [Call] {
         get {
@@ -42,7 +44,20 @@ public class CallManager : NSObject, CallDelegate {
         }
     }
     
-    public init(notificationManager : NotificationManager? = nil) {
+    public init(notificationManager : NotificationManager? = nil, telephonyService : SystemTelephonyAdapter? = nil) {
+        if telephonyService != nil {
+            self.telephonyService = telephonyService!
+        }
+        
+        let os = ProcessInfo().operatingSystemVersion
+        if os.majorVersion >= 10 {
+            Log.debug?.message("iOS 10 or higher. Using CallKit.")
+            self.telephonyService = CallKitCallController()
+        } else {
+            Log.debug?.message("Pre iOS 10. Falling back to old VoIP frameworks")
+            fatalError("Not implemented yet")
+        }
+        
         super.init()
         
         if notificationManager == nil {
@@ -79,7 +94,13 @@ public class CallManager : NSObject, CallDelegate {
             Log.error?.message("Call with UUID \(inCall.uuid) connected, but no such call exists!")
             return
         }
-        call.isActive = true
+                
+        do {
+            try telephonyService?.startCall(inCall)
+            call.isActive = true
+        } catch {
+            Log.error?.message("Unable to start call: \(inCall). Error: \(error)")
+        }
     }
     
     public func callDidDisconnect(_ inCall : Call){
@@ -87,7 +108,13 @@ public class CallManager : NSObject, CallDelegate {
             Log.error?.message("Call with UUID \(inCall.uuid) disconnected, but no such call exists!")
             return
         }
-        call.isActive = false
+        
+        do {
+            try telephonyService?.stopCall(inCall)
+            call.isActive = false
+        } catch {
+            Log.error?.message("Unable to stop call: \(inCall). Error: \(error)")
+        }
     }
     
     public func callCancelled(_ inCall : Call){
@@ -95,11 +122,26 @@ public class CallManager : NSObject, CallDelegate {
             Log.error?.message("Call with UUID \(inCall.uuid) was cancelled, but no such call exists!")
             return
         }
-        call.isActive = false
+        
+        
+        do {
+            try telephonyService?.stopCall(inCall)
+            call.isActive = false
+        } catch {
+            Log.error?.message("Unable to stop call: \(inCall). Error: \(error)")
+        }
+
     }
     
     public func callReceived(_ inCall : Call){
         incomingCalls.append(inCall)
+        
+        do {
+            try telephonyService?.handleIncomingCall(inCall)
+        } catch {
+            Log.error?.message("Unable to handle incoming call: \(inCall). Error: \(error)")
+        }
+
     }
     
     public func call(_ inCall : Call, didFailWithError : Error){
